bbfe6e768e1cc933220aea87a9de6a2839595a39
Jack Song <133342301@163.com>
2020-04-19 16:24:24 +1000

    Added Missing File

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/story.js'],
+  addons: [
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-viewport/register'
+  ],
+};
